84a6adc96123c48abbff8740ff3707c0df56dacc
Michael Bard <michael1bard@gmail.com>
2023-08-03 01:20:52 -0400

    add product sorting filtering

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,7 @@
 // Put your application javascript here
 // alert("I have an alert box");
+document.querySelector("#sort_by").addEventListener("change", function (e) {
+  var url = new URL(window.location.href);
+  url.searchParams.set("sort_by", e.currentTarget.value);
+  window.location = url.href;
+});
